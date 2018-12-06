ea43598ac6b39429409c7372af677a33481bff61
Dylan Khor <thekhord@gmail.com>
2018-12-06 16:38:15 -0500

    add powershell profile for aliases

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+Set-Alias cvis Connect-VIServer
+function dvisf {Disconnect-VIServer * -cf:$false}
+Set-Alias dvis dvisf
+function gvisf {$global:DefaultVIServers}
+Set-Alias gvis gvisf
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-ls -sf /Users/khord/dotfiles/.zshrc /Users/khord/.zshrc
-ls -sf /Users/khord/dotfiles/.vimrc /Users/khord/.vimrc
-brew bundle
+ls -sf ~/dotfiles/.zshrc ~/.zshrc
+ls -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/Microsoft.PowerShell_profile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
+#brew bundle
