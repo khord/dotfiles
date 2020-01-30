30d0c86dce37baa1d80ab8e043c9abff6bd7d573
Dylan Khor <thekhord@gmail.com>
2020-01-30 13:38:34 -0500

    brewfile, powercli consolidation, iterm2 settings

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,4 +5,6 @@ function dvisf {Disconnect-VIServer * -cf:$false}
 Set-Alias dvis dvisf
 function gvisf {$global:DefaultVIServers}
 Set-Alias gvis gvisf
+function consolidationf {(Get-View -ViewType VirtualMachine -Property Name, Runtime.ConsolidationNeeded | ?{$_.Runtime.ConsolidationNeeded}).Count}
+Set-Alias consolidation consolidationf
 cd /Users/khord/repos/simspace/VMware-Automation
